0af26727809474b91f8f81321a0b5dacdddc4886
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-02-08 19:41:53 -0300

    Exercício 2 - Aula 2

---
+++ Aula 2/Exercícios/Exercício 2/script.js
@@ -0,0 +1,22 @@
+function calcularIMC() {
+    var peso = document.getElementById("peso").value;
+    var altura = document.getElementById("altura").value;
+    var imc = peso / (altura * altura);
+    var situacao = "";
+    
+    if (imc < 20) {
+      situacao = "Abaixo do Peso";
+    } else if (imc >= 20 && imc <= 25) {
+      situacao = "Normal";
+    } else if (imc > 25 && imc <= 30) {
+      situacao = "Sobrepeso";
+    } else if (imc > 30 && imc <= 35) {
+      situacao = "Obesidade grau I";
+    } else if (imc > 35 && imc <= 40) {
+      situacao = "Obesidade grau II";
+    } else {
+      situacao = "Obesidade grau III";
+    }
+    
+    document.getElementById("resultado").innerHTML = "IMC: " + imc + "<br> Situação: " + situacao;
+}

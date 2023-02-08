0dab2b2c83cfc943f32aa34f49a6f75e218b28c1
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-02-08 20:04:14 -0300

    Exercício 4 - Aula 2

---
+++ Aula 2/Exercícios/Exercício 4/script.js
@@ -0,0 +1,30 @@
+function verificarPreco() {
+    var codigo = parseInt(document.getElementById("codigo").value);
+    var preco;
+    var produto;
+    
+    switch (codigo) {
+      case 1:
+        preco = "R$ 99,99";
+        produto = "Sapato";
+        break;
+      case 2:
+        preco = "R$ 103,89";
+        produto = "Bolsa";
+        break;
+      case 3:
+        preco = "R$ 49,98";
+        produto = "Camisa";
+        break;
+      case 4:
+        preco = "R$ 89,72";
+        produto = "Calça";
+        break;
+      default:
+        preco = "";
+        produto = "Código Inválido";
+        break;
+    }
+
+    document.getElementById("resultado").innerHTML = "O preço do produto " + produto + " é " + preco;
+}

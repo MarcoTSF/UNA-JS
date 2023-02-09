a3fef6ba12de7fe2bfe2cc8d6e1a823e42609920
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-02-09 20:38:58 -0300

    Correção Professor

--- Aula 2/Exercícios/Exercício 4/script.js
+++ Aula 2/Exercícios/Exercício 4/script.js
@@ -32,3 +32,48 @@ function verificarPreco() {
 
     document.getElementById("resultado").innerHTML = "O preço do produto " + produto + " é " + preco;
 }
+
+/*
+------- Correção do Professor ------------
+
+
+var codigo = parseInt(prompt("Informe o código do produto:", "código"));
+    
+if (codigo == 1)
+    document.write("Sapato: R$ 99,99");
+else if (codigo == 2)
+    document.write("Bolsa: R$ 103,89");
+else if (codigo == 3)
+    document.write("Camisa: R$ 49,98");
+else if (codigo == 4)
+    document.write("Calça: R$ 89,72");
+else if (codigo == 5)
+    document.write("Blusa: R$ 97,35");
+else document.write("Código incorreto!");
+
+
+// ------- Correção do Professor 2 ------------
+
+
+var codigo = parseInt(prompt("Informe o código do produto:", "código"));
+    
+switch(codigo){
+  case 1:
+    document.write("Sapato: R$ 99,99");
+    break;
+  case 2:
+    document.write("Bolsa: R$ 103,89");
+    break;
+  case 3:
+    document.write("Camisa: R$ 49,98");
+    break;
+  case 4:
+    document.write("Calça: R$ 89,72");
+    break;
+  case 5:
+    document.write("Blusa: R$ 97,35");
+    break;
+  default:
+    document.write("Código incorreto!");
+}
+*/
--- Aula 2/Exercícios/Exercício 5/script.js
+++ Aula 2/Exercícios/Exercício 5/script.js
@@ -39,3 +39,29 @@ function calcularSalario() {
 
     document.getElementById("resultado").innerHTML = "Nome: " + nome + "<br> Valor da hora: R$ " + valorHora.toFixed(2) + "<br> Salário bruto: R$ " + salarioBruto.toFixed(2);
 }
+
+
+// ----------- Correção do Professor ------------
+
+
+var codigo = parseInt(prompt("Informe o código do(a) colaborador(a):", "")),
+    horas = parseInt(prompt("Informe as horas trabalhadas:", ""));
+    
+switch (codigo){
+    case 1:
+    document.write(`Ana: ${horas} * 45,78 = ${horas * 45.78}.`);
+    break
+    case 2:
+    document.write(`Bruna: ${horas} * 60,00 = ${(horas * 60).toPrecision(5)}.`);
+    break;    
+    case 3:
+    document.write(`Carlos: ${horas} * 38,99 = ${horas * 38.99}.`);
+    break;    
+    case 4:
+    document.write(`Diogo: ${horas} * 45,78 = ${horas * 45.78}.`);
+    break;    
+    case 5:
+    document.write(`Ester: ${horas} * 45,78 = ${horas * 45.78}.`);
+    break;    
+    default:  document.write("Código incorreto!");
+}
